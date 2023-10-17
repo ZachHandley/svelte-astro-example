721ce14fc3d83c075cea1468819424bf6935f199
ZachHandley <zachhandley@gmail.com>
2023-10-16 23:45:36 -0700

    Initial commit

---
+++ src/components/Base.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { Canvas } from "@threlte/core";
+import Scene from "./Scene.svelte";
+import { someAtom } from "./Store.ts";
+
+const changeTheATOM = () => {
+  const value = someAtom.get();
+  if (value === 'oohyeah') {
+    someAtom.set('ouchthathurts');
+  } else {
+    someAtom.set('oohyeah');
+  }
+}
+</script>
+
+<div style="width: 100vw; height: 100vh;">
+  <Canvas>
+    <Scene />
+  </Canvas>
+  <button on:click={changeTheATOM()} style="width: 25%; background-color: white; color: black; position: absolute; left: 50%; transform: translateX(-50%); border-radius: 8px;">Click Meeee</button>
+</div>
---
+++ src/components/Scene.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { T, useFrame } from '@threlte/core'
+  import { interactivity } from '@threlte/extras'
+  import { spring } from 'svelte/motion'
+  import { someAtom } from "./Store.ts"
+
+  interactivity()
+  $: someReactiveValue = 1
+  const scale = spring(someReactiveValue)
+  let rotation = 0
+  useFrame((state, delta) => {
+    rotation += delta
+  })
+
+  someAtom.subscribe((newValue) => {
+    if (newValue === 'oohyeah') {
+      someReactiveValue = 5;
+    } else {
+      someReactiveValue = 1;
+    }
+  })
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[10, 10, 10]}
+  on:create={({ ref }) => {
+    ref.lookAt(0, 1, 0)
+  }}
+/>
+
+<T.DirectionalLight position={[0, 10, 10]} castShadow />
+
+<T.Mesh
+  rotation.y={rotation}
+  position.y={1}
+  scale={$scale}
+  on:pointerenter={() => scale.set(1.5)}
+  on:pointerleave={() => scale.set(1)}
+  castShadow
+>
+  <T.BoxGeometry args={[1, 2, 1]} />
+  <T.MeshStandardMaterial color="hotpink" />
+</T.Mesh>
+
+<T.Mesh rotation.x={-Math.PI/2} receiveShadow>
+  <T.CircleGeometry args={[4, 40]}/>
+  <T.MeshStandardMaterial color="white" />
+</T.Mesh>
